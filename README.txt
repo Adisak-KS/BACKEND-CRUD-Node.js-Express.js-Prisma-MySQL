50a5fb14a91a430517f7b2bb86fa84ab52e0df04
Adisak-KS <Adisak.General@gmail.com>
2024-10-28 12:54:04 +0700

    use prisma

---
+++ README.txt
@@ -0,0 +1,1 @@
+password adisak123456
--- server/controllers/product.js
+++ server/controllers/product.js
@@ -1,24 +1,48 @@
+const prisma = require('../prisma/prisma')
+
+
+
 exports.create = async (req, res) => {
     try {
-        const {name, price} = req.body
-        console.log(name)
-        res.send('Hello Controller Create')
+        const { name, price } = req.body
+        const newProduct = await prisma.product.create({
+            data: {
+                name: name,
+                price: price
+            }
+        })
+
+
+
+
+        res.send(newProduct)
     } catch (err) {
         console.log(err)
+        res.send('Server Error').status(500)
     }
 }
 
 exports.list = async (req, res) => {
     try {
-        res.send('Hello Controller List(GET)')
+        const listProduct = await prisma.product.findMany()
+
+        res.send(listProduct)
     } catch (err) {
         console.log(err)
+        res.send('Server Error').status(500)
     }
 }
 
 exports.read = async (req, res) => {
     try {
-        res.send('Hello Controller Read (GET:ID)')
+        const { productId } = req.params
+        const product = await prisma.product.findUnique({
+            where: {
+                id: Number(productId),
+            }
+        })
+
+        res.json(product)
     } catch (err) {
         console.log(err)
     }
@@ -26,16 +50,36 @@ exports.read = async (req, res) => {
 
 exports.update = async (req, res) => {
     try {
-        res.send('Hello Controller Update (PUT:ID)')
+        const { productId } = req.params
+        const { name, price } = req.body
+        const newProduct = await prisma.product.update({
+            where: {
+                id: Number(productId),
+            },
+            data: {
+                name: name,
+                price: price
+            }
+        })
+
+        res.json(newProduct)
     } catch (err) {
         console.log(err)
+        res.status(500).json('Server Error')
     }
 }
 
 exports.remove = async (req, res) => {
     try {
-        res.send('Hello Controller Remove (DELETE:ID)')
+        const { productId } = req.params
+        const deletedProduct = await prisma.product.delete({
+            where: {
+                id: Number(productId),
+            }
+        })
+        res.json(deletedProduct)
     } catch (err) {
         console.log(err)
+        res.status(500).json('Server Error')
     }
 }
